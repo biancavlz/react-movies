457581e97c9ca665676604b9654f02bed7151aa7
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-12 13:52:00 +0100

    Use async/wait pattern that .then chaing

--- src/App.js
+++ src/App.js
@@ -54,10 +54,18 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
 
+  const query = "interstellar";
+
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+      );
+
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (

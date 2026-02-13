d11083b3f06c87b83786be4aa3c51bd6b447ef3a
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-13 10:56:32 +0100

    set back states when no query

--- src/App.js
+++ src/App.js
@@ -84,6 +84,14 @@ export default function App() {
           setIsLoading(false);
         }
       }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+
+        return;
+      }
+
       fetchMovies();
     },
     [query],

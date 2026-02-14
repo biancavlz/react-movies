a5fbd3d06b662597d6f690643054288184b9156a
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-14 11:32:14 +0100

    Avoid add to the list same movie

--- src/App.js
+++ src/App.js
@@ -108,6 +108,7 @@ export default function App() {
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
               onAddWatched={handleWatched}
+              watched={watched}
             />
           ) : (
             <>
@@ -121,10 +122,12 @@ export default function App() {
   );
 }
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
 
   const {
     Title: title,
@@ -197,11 +200,21 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className="rating">
-              <StarRating maxRating={10} size={24} onSetRaing={setUserRating} />
-              {userRating > 0 && (
-                <button className="btn-add" onClick={handleAddWatchedMovie}>
-                  + add to list
-                </button>
+              {!isWatched ? (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRaing={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAddWatchedMovie}>
+                      + add to list
+                    </button>
+                  )}
+                </>
+              ) : (
+                <p>You have already rated this movie</p>
               )}
             </div>
             <p>

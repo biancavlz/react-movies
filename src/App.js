e06c83b6004e48ce8ca10c16408d53d501558cd8
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-16 15:26:02 +0100

    add number of ratings decitions with useRef hook

--- src/App.js
+++ src/App.js
@@ -134,6 +134,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const ratingCountRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) ratingCountRef.current = ratingCountRef.current + 1;
+    },
+    [userRating],
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId,
@@ -208,6 +217,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: ratingCountRef.current,
     };
 
     onAddWatched(newWatchedMovie);

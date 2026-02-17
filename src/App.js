938b1371f536d56075b011bd5b8d6d623ffde52f
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-17 09:26:44 +0100

    create useLocalStorageState custom hook and apply

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import StarRating from "./components/StarRating";
 import { useMovies } from "./hooks/useMovies";
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -9,11 +10,9 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedID] = useState(null);
   const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched");
-    return storedValue ? JSON.parse(storedValue) : [];
-  });
   // const [watched, setWatched] = useState([]);
+
+  const [watched, setWatched] = useLocalStorageState([], "watched");
 
   function handleSelectMovie(id) {
     setSelectedID((selectedId) => (id === selectedId ? null : id));
@@ -31,13 +30,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched],
-  );
 
   return (
     <>
